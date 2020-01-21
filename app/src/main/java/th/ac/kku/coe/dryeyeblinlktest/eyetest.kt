d39addedf33cd05c0c0c887aa14b1d399df4ca89
42835308 <nattapat.p@outlook.co.th>
2020-01-21 22:45:39 +0700

    Lab 5

---
+++ app/src/main/java/th/ac/kku/coe/dryeyeblinlktest/eyetest.kt
@@ -0,0 +1,12 @@
+package th.ac.kku.coe.dryeyeblinlktest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class eyetest : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eyetest)
+    }
+}
---
+++ app/src/main/java/th/ac/kku/coe/dryeyeblinlktest/menus.kt
@@ -0,0 +1,14 @@
+package th.ac.kku.coe.dryeyeblinlktest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class menus : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menus)
+
+
+    }
+}
